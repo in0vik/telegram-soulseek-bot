d2491ccde0582f04a9e11cb502a45c13b80c46fe
“inovik94” <ilya.novikk@gmail.com>
2022-02-01 20:43:10 +0300

    update msg

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const _ = require('lodash');
-const slsk = require('slsk-client');
-const Telegraf = require('telegraf');
+const _ = require("lodash");
+const slsk = require("slsk-client");
+const Telegraf = require("telegraf");
 
 const FILTER_WORDS = [
-  'remix', 'rmx', 'edit', 'cover', 'live', 'mix', 'bootleg', 'acapella',
-  'mashup',
+  "remix",
+  "rmx",
+  "edit",
+  "cover",
+  "live",
+  "mix",
+  "bootleg",
+  "acapella",
+  "mashup",
 ];
 
 const humanFilesize = (size) => {
-  var i = Math.floor( Math.log(size) / Math.log(1024) );
-  return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+  var i = Math.floor(Math.log(size) / Math.log(1024));
+  return (size / Math.pow(1024, i)).toFixed(2) * 1 + " " + ["B", "kB", "MB", "GB", "TB"][i];
 };
 
 const handleErr = (ctx, err) => {
@@ -35,23 +42,23 @@ const basename = (filename) => {
 
 const filterResult = (r, query) => {
   const filename = basename(r.file);
-  const splitQuery = query.split(' - ');
-  const splitFilename = filename.split(' - ');
+  const splitQuery = query.split(" - ");
+  const splitFilename = filename.split(" - ");
 
-  return (r.bitrate >= 320
-          && r.file.endsWith('.mp3')
-          && _.every(splitQuery, (piece, i) => {
-            try {
-              return splitFilename[i].toLowerCase().includes(piece.toLowerCase());
-            } catch (e) {
-              return false;
-            }
-          })
-          && _.every(FILTER_WORDS, (word) => {
-            return !filename.toLowerCase().includes(word)
-              || query.toLowerCase().includes(word)
-          })
-         )
+  return (
+    r.bitrate >= 320 &&
+    r.file.endsWith(".mp3") &&
+    _.every(splitQuery, (piece, i) => {
+      try {
+        return splitFilename[i].toLowerCase().includes(piece.toLowerCase());
+      } catch (e) {
+        return false;
+      }
+    }) &&
+    _.every(FILTER_WORDS, (word) => {
+      return !filename.toLowerCase().includes(word) || query.toLowerCase().includes(word);
+    })
+  );
 };
 
 const formatResult = (r) => {
@@ -59,38 +66,53 @@ const formatResult = (r) => {
 };
 
 const retrieveFile = (soulseek, ctx, result, filename) => {
-  const downloadPath = __dirname + '/download/' + filename;
-  soulseek.download({
-    file: result,
-    path: downloadPath,
-  }, (err, data) => {
-    if (err) { handleErr(ctx, err); }
-    sendMessage(ctx, `Download of "${downloadPath}" completed!`);
-  });
-}
+  const downloadPath = __dirname + "/download/" + filename;
+  soulseek.download(
+    {
+      file: result,
+      path: downloadPath,
+    },
+    (err, data) => {
+      if (err) {
+        handleErr(ctx, err);
+      }
+      sendMessage(ctx, `Download of "${downloadPath}" completed!`);
+      sendMessage(ctx, `test`);
+    }
+  );
+};
 
 const onDownload = async (soulseek, ctx, query) => {
   sendMessage(ctx, `Searching: ${query}`);
-  const req = query.toLowerCase().replace(' - ', ' ');
+  const req = query.toLowerCase().replace(" - ", " ");
   soulseek.search({ req, timeout: 20000 }, (err, rawResults) => {
-    if (err) { handleErr(ctx, err); }
-    const sorted = _.sortBy(rawResults, ['speed', 'slots']);
-    const filtered = sorted.filter((r) => { return filterResult(r, query) });
+    if (err) {
+      handleErr(ctx, err);
+    }
+    const sorted = _.sortBy(rawResults, ["speed", "slots"]);
+    const filtered = sorted.filter((r) => {
+      return filterResult(r, query);
+    });
     if (filtered.length === 0) {
-      const resultsString = _.map(sorted, formatResult).join('\n');
+      const resultsString = _.map(sorted, formatResult).join("\n");
       sendMessage(ctx, `Found 0 results (${rawResults.length} unfiltered)\n\n${resultsString}`);
       return;
     }
     const bestResult = filtered[filtered.length - 1];
-    sendMessage(ctx, `Found ${filtered.length} results (${rawResults.length} unfiltered)\nBest result: ${formatResult(bestResult)}`);
+    sendMessage(
+      ctx,
+      `Found ${filtered.length} results (${
+        rawResults.length
+      } unfiltered)\nBest result: ${formatResult(bestResult)}`
+    );
     retrieveFile(soulseek, ctx, bestResult, `${query}.mp3`);
   });
-}
+};
 
 const main = async (soulseek) => {
   console.log("Starting...");
   const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
-  bot.on('text', (ctx) => {
+  bot.on("text", (ctx) => {
     const query = ctx.update.message.text.trim();
     onDownload(soulseek, ctx, query);
   });
@@ -98,11 +120,15 @@ const main = async (soulseek) => {
 };
 
 (async () => {
-  slsk.connect({
-    user: process.env.SLSK_USER,
-    pass: process.env.SLSK_PASS,
-  }, async (err, client) => {
-    await main(client);
-  });
-})().catch(err => { console.error(err) });
-
+  slsk.connect(
+    {
+      user: process.env.SLSK_USER,
+      pass: process.env.SLSK_PASS,
+    },
+    async (err, client) => {
+      await main(client);
+    }
+  );
+})().catch((err) => {
+  console.error(err);
+});
